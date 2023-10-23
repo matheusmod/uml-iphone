14ba2d4d3f47f9c9fae416f5c95cf6ea158d232b
matheusmod <146960373+matheusmod@users.noreply.github.com>
2023-10-22 21:47:36 -0300

    Create NavegadorInternet.java

---
+++ NavegadorInternet.java
@@ -0,0 +1,7 @@
+
+public interface NavegadorInternet {
+    void navegarInternet();
+    void adicionarPagina(String pagina);
+    void exibirPagina(String pagina);
+    void atualizarPagina(String pagina);
+}
